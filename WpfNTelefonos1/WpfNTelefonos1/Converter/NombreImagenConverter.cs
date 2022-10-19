9005401193bf86a9819972168c0ef26d32749b42
Alberto Luján <93909889+AlbertoLujanCandenas@users.noreply.github.com>
2022-10-19 18:49:40 +0200

    Final version

---
+++ WpfNTelefonos1/WpfNTelefonos1/Converter/NombreImagenConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfNTelefonos1.Converter
+{
+    public class NombreImagenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Nos permite transformar un objeto de una tipo específico a uno nuevo. En este caso lo transformamos en un Bitmapimage
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+
+            b.UriSource = new Uri(string.Format("..\\..\\Assets\\{0}", (string)value), UriKind.RelativeOrAbsolute);
+            b.EndInit();
+
+            return b;
+            
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
